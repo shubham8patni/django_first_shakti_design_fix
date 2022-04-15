08b72d3810bbc8870f0afd08db996a9b67fef478
Shakti09-Rookie <shaktidagar09@gmail.com>
2022-04-15 18:00:58 +0530

    commit before heroku

--- users/apps.py
+++ users/apps.py
@@ -1,7 +1,8 @@
 from django.apps import AppConfig
-
+from django.db import models
 
 class UsersConfig(AppConfig):
+    default_auto_field = ('django.db.models.BigAutoField')
     name = "users"
 
     def ready(self):
