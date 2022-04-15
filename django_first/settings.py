30ec2719e207f0dcc9102edb0718c5da77aeae17
Shakti09-Rookie <shaktidagar09@gmail.com>
2022-04-15 18:50:40 +0530

    updated settings

--- django_first/settings.py
+++ django_first/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "hnc*cxh!+b+q2(9m%a31a7@&z%b6qp9$#qfb(_$%@!e1jww$0y"
+# SECRET_KEY = "hnc*cxh!+b+q2(9m%a31a7@&z%b6qp9$#qfb(_$%@!e1jww$0y"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['firstdjangoblogcs.herokuapp.com']
 
 
 # Application definition
